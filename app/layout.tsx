9549cae0705e1b28b2ca6ae7623dbaff84a73079
lokhacodes <sumaiyasonali421@gmail.com>
2026-05-26 23:16:16 +0600

    Image Optimizations-Linking-navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css";
+import Link from "next/link"
+import type { RootLayoutProps } from "@/app/types"
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google"
+import PFLogoIcon from "@/public/printforge-logo-icon.svg"
+import PFLogo from "@/public/printforge-logo.svg"
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -13,33 +17,35 @@ const montserratAlternates = Montserrat_Alternates({
   variable: "--font-montserrat-alternates"
 })
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={`${albertSans.className} ${montserratAlternates.variable}`}>
         <header className="w-full bg-white">
           <nav className="flex justify-between px-6 py-4">
-            <div className="relative">
-              {/* Desktop Logo */}
-              <img
-                src={"/printforge-logo.svg"}
-                alt="PrintForge Logo"
-                className="w-[200px] h-auto hidden md:block"
-              />
-              {/* Mobile Logo */}
-              <img
-                src={"/printforge-logo-icon.svg"}
-                alt="PrintForge Logo"
-                className="w-[40px] h-auto block md:hidden"
-              />
-            </div>
+            <Link href="/">
+              <div className="relative cursor-pointer">
+                {/* Desktop Logo */}
+                <img
+                  src={PFLogo.src}
+                  alt="PrintForge Logo"
+                  className="w-[200px] h-auto hidden md:block"
+                />
+                {/* Mobile Logo */}
+                <img
+                  src={PFLogoIcon.src}
+                  alt="PrintForge Logo"
+                  className="w-[40px] h-auto block md:hidden"
+                />
+              </div>
+            </Link>
             <ul className="flex items-center gap-2.5">
-              <p>3D Models</p>
-              <p href="/about">About</p>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/3d-models">3D Models</Link>
+              </li>
+              <li className="text-sm uppercase cursor-pointer">
+                <Link href="/about">About</Link>
+              </li>
             </ul>
           </nav>
         </header>
